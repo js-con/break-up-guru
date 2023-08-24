615b1c22bad2a4503529edfc0f15292a3e2a214c
004725 <linbowen@fcbox.com>
2023-08-24 14:44:19 +0800

    ✨ feat: page structure

---
+++ src/pages/main/scale/index.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import scaleMap from '@/hooks/scales/index.ts'
+
+const scaleData = ref()
+
+onLoad((options: any) => {
+  const scale = options.scale
+  if (scale && scaleMap[scale])
+    scaleData.value = scaleMap[scale]()
+})
+</script>
+
+<template>
+  <view>
+    <view v-if="scaleData">
+      <view>{{ scaleData.describe }}</view>
+    </view>
+  </view>
+</template>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <view>mine</view>
+</template>
